e136a863b701e4e2969a8327050915a57c3fa3a2
Hkondza <hkondza@algebra.hr>
2026-05-06 22:26:05 +0200

    Android Login/Register

---
+++ andorid/app/build/generated/source/buildConfig/debug/hr/team16/booksy/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package hr.team16.booksy;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "hr.team16.booksy";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String API_KEY = "e4e66c2d8fb6475be8ba71dd2c1f33b8";
+}
